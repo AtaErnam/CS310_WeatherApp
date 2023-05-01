e31353d7f5caf4bf45340df8116834e476cb27f9
Ata Ernam <ataernam@sabanciuniv.edu>
2023-05-01 12:54:55 +0300

    Initialized project

---
+++ src/test/java/com/example/CS310_WEATHERAPP/Cs310WeatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CS310_WEATHERAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs310WeatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
